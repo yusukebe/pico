fb0123d5a04eaa9c3e10144eb7030cd249d7fbd9
Yusuke Wada <yusuke@kamawada.com>
2022-11-25 00:02:58 +0900

    chore: update example

--- examples/worker.ts
+++ examples/worker.ts
@@ -3,8 +3,8 @@ import { Pico } from '../src/index'
 const app = new Pico()
 
 app.get('/', () => 'Hello Pico!')
-app.get('/entry/:id', ({ pathname }) => {
-  const { id } = pathname.groups
+app.get('/entry/:id', ({ params }) => {
+  const { id } = params
   return { 'your id is': id }
 })
 
