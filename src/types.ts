1717ab569226c76d58b76a7f7d8ed4ef63443db3
Yusuke Wada <yusuke@kamawada.com>
2022-11-25 00:00:31 +0900

    docs(readme): add description for `env` and `executionContext`

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,7 @@
 export type Handler = (context: Context) => Response | string | object
 export interface Context {
   request: Request
-  env: object
+  env: Record<string, any>
   executionContext: ExecutionContext
   params: Record<string, string>
   get url(): URL
